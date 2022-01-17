4414594e953940d253f5e73d3622dd7488e1fbfa
lphlch <lvpinhao@sohu.com>
2022-01-17 16:28:20 +0800

    Revert "Dir name change"
    This reverts commit 857006c06fb66464bbff4f8b08093ab8b57064d7.

--- Digital-Logic/Vivado/02-Experiment1/logic_gates_1.v
+++
@@ -1,7 +0,0 @@
-module logic_gates_1(iA, iB, oAnd, oOr, oNot);
-    input iA,iB;
-    output oAnd, oOr, oNot;
-    and and_inst(oAnd, iA, iB);
-    or or_inst(oOr, iA, iB);
-    not not_inst(oNot, iA);
-endmodule
--- Digital-Logic/Vivado/02-Experiment1/logic_gates_2.v
+++
@@ -1,7 +0,0 @@
-module logic_gates_2(iA, iB, oAnd, oOr, oNot);
-    input iA, iB;
-    output oAnd, oOr, oNot;
-    assign oAnd=iA & iB;
-    assign oOr= iA | iB;
-    assign oNot=~iA;
-endmodule
